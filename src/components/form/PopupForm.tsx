aabec1d2d4fcfe20926857d121ab158b58a68124
meselghea <122159476+meselghea@users.noreply.github.com>
2023-06-12 21:58:32 +0700

    fix closepopup

--- src/components/form/PopupForm.tsx
+++ src/components/form/PopupForm.tsx
@@ -25,9 +25,6 @@ const PopupForm: React.FC<PopupFormProps> = ({ onClose, children }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-opacity-50/50">
       <div ref={popupRef} className="max-w-[600px] min-h-[300px] bg-white rounded-lg flex flex-col items-center justify-center">
         <button className="absolute text-gray-500 top-4 right-4 hover:text-gray-700" onClick={onClose}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-          </svg>
         </button>
         {children}
       </div>
